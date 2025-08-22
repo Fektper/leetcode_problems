8cf1ad665ec04fa581a920119c61fe47bc750261
Rodna <andor.kristen@web.de>
2025-08-22 22:52:50 +0200

    Second version, restricted range of loops

--- 3195/c++/solution.cpp
+++ 3195/c++/solution.cpp
@@ -13,26 +13,55 @@ public:
         min_left = min_top = std::max(row_count, col_count);
         max_right = max_bot = 0;
 
+        // Find first row with 1s and last row with 1s
+        bool row_has_one = false;
         for (int row = 0; row < row_count; row++){
             for (int col = 0; col < col_count; col++){
-                if (!grid.at(row).at(col)){
-                    continue;
-                }
-
-                if (col < min_left){
+                if (grid.at(row).at(col)){
+                    row_has_one = true;
+                    min_top = row; // This is final
                     min_left = col;
-                }
-                if (col > max_right){
                     max_right = col;
+                    break;
                 }
-
-                if (row < min_top){
-                    min_top  = row;
-                }
-                max_bot = row;
+            }
+            if (row_has_one){
+                break;
             }
         }
 
+        row_has_one = false;
+        for (int row = row_count - 1; row >= 0; row--){
+            for (int col = 0; col < col_count; col++){
+                if (grid.at(row).at(col)){
+                    row_has_one = true;
+                    max_bot = row; // This is final
+                    min_left = std::min(col, min_left);
+                    max_right = std::max(col, max_right);
+                    break;
+                }
+            }
+            if (row_has_one){
+                break;
+            }
+        }
+        
+        // min_top and max_bot set, left/right need to be optimised
+
+        for (int row = min_top; row <= max_bot; row ++){
+            for (int col = 0; col < min_left; col ++){
+                if (grid.at(row).at(col)){
+                    min_left = col;
+                    break;
+                }
+            }
+            for (int col = col_count - 1; col > max_right; col--){
+                if (grid.at(row).at(col)){
+                    max_right = col;
+                    break;
+                }
+            }
+        }
         return (max_right - min_left + 1) * (max_bot - min_top + 1);
     }
 };
