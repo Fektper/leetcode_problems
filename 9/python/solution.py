dc806785793908e16db6ac66e9b298be788e3f56
Rodna <andor.kristen@web.de>
2025-08-23 23:22:40 +0200

    'Optimized' version, negatvie values are directly discarded, beats 93% :)

--- 9/python/solution.py
+++ 9/python/solution.py
@@ -2,28 +2,30 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
+        # if x < 0:
+        #     return False
+        
+        # digits = []
+        # while x > 0:
+        #     digits.append(x%10)
+        #     x = x // 10
+
+        # digits_num = len(digits)
+        # for i in range(0, digits_num // 2):
+        #     if not digits[i] == digits[digits_num - 1 - i]:
+        #         return False
+            
+        # return True
+
         if x < 0:
             return False
-        
-        digits = []
-        while x > 0:
-            digits.append(x%10)
-            x = x // 10
+        num_string = str(x)
+        digits = len(num_string)
 
-        digits_num = len(digits)
-        for i in range(0, digits_num // 2):
-            if not digits[i] == digits[digits_num - 1 - i]:
+        for i in range(0, digits // 2):
+            if not num_string[i] == num_string[digits - 1 - i]:
                 return False
-            
         return True
-
-        # num_string = str(x)
-        # digits = len(num_string)
-
-        # for i in range(0, digits // 2):
-        #     if not num_string[i] == num_string[digits - 1 - i]:
-        #         return False
-        # return True
     
     
 
