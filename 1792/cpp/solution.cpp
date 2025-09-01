6131a1caf37c520bbd2e38390602ea036dadb960
Rodna <andor.kristen@web.de>
2025-09-01 12:13:27 +0200

    Keeps list of gains, only update the one that is changed. Still too slow, lookup of max gain takes O(n) in every step. We need constant time max lookup (Heap).

--- 1792/cpp/solution.cpp
+++ 1792/cpp/solution.cpp
@@ -1,19 +1,18 @@
 #include <vector>
 
+
 using namespace std;
 
-int findMinTakersAvailable(vector<vector<int>>& classes){
+int maxIndex(vector<double>& vals){
+    int idx = 0;
+    double maxVal = vals[0];
 
-    double maxGain = -1;
-    int idx = -1;
-
-    double gain;
-    for (int i = 0; i < classes.size(); i++){
-        gain = ((double) classes[i][1] + 1) / ((double) classes[i][0] + 1) - ((double) classes[i][1]) / ((double) classes[i][0]);
-        if (gain > maxGain){
-            maxGain = gain;
+    for (int i = 1; i < vals.size(); i++){
+        if (vals[i] > maxVal){
+            maxVal = vals[i];
             idx = i;
         }
+    }
     return idx;
 }
 
@@ -21,12 +20,19 @@ class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
 
+        vector<double> gains(classes.size());
+
+        for (int i = 0; i < classes.size(); i++){
+            gains[i] = ((double) classes[i][0] + 1) / ((double) classes[i][1] + 1) - ((double) classes[i][0]) / ((double) classes[i][1]);
+        }
+
         int i;
         while (extraStudents > 0){
-            i = findMinTakersAvailable(classes);
+            i = maxIndex(gains);
             classes[i][0]++;
             classes[i][1]++;
             extraStudents--;
+            gains[i] = ((double) classes[i][0] + 1) / ((double) classes[i][1] + 1) - ((double) classes[i][0]) / ((double) classes[i][1]);
         }
 
         double res = 0;
