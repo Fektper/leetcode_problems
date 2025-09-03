2a63b4b8f1543a0655669e0e93a743b4706c49ef
Rodna <andor.kristen@web.de>
2025-09-03 19:58:22 +0200

    Optimized from O(n^2) to O(nlog(n)) by using high and low priority queue of elements from vector. Beats 100 % in 0ms.

--- 1/cpp/solution.cpp
+++ 1/cpp/solution.cpp
@@ -1,17 +1,53 @@
 #include <vector>
+#include <queue>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
+
+
+class Smaller{
+public:
+    bool operator() (tuple<int, int> a, tuple<int, int> b){
+        return get<0>(a) < get<0>(b);
+    }
+};
+class Greater{
+public:
+    bool operator() (tuple<int, int> a, tuple<int, int> b){
+        return get<0>(a) > get<0>(b);
+    }
+};
+
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size()-1; i++){
-            for (int j = i+1; j < nums.size(); j++){
-                if (nums[i] + nums[j] == target){
-                    return {i, j};
-                }
+        priority_queue<tuple<int, int>, vector<tuple<int, int>>, Smaller> high_elements;
+        priority_queue<tuple<int, int>, vector<tuple<int, int>>, Greater> low_elements;
+        for (int i = 0; i < nums.size(); i++){
+            high_elements.push({nums[i], i});
+            low_elements.push({nums[i], i});
+        }
+
+        tuple<int, int> first;
+        tuple<int, int> second;
+        while (!high_elements.empty() && !low_elements.empty()){
+            first = high_elements.top();
+            second = low_elements.top();
+            if (get<0>(first) + get<0>(second) > target){
+                high_elements.pop();
+            }
+            else if (get<0>(first) + get<0>(second) > target){
+                low_elements.pop();
+            }
+            else if (get<1>(first) == get<1>(second)){
+                low_elements.pop();
+            }
+            else {
+                return {get<1>(first), get<1>(second)};
             }
         }
+        return {0,0};
     }
 };
