d111f8a106f015820dbb3e85c1454ddc2e240663
Rodna <andor.kristen@web.de>
2025-09-28 23:06:54 +0200

    Direct access to array objects instead of intermediate store. Beats 100%.

--- 0976/cpp/solution.cpp
+++ 0976/cpp/solution.cpp
@@ -8,13 +8,9 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int a; int b; int c;
         for (int i = nums.size()-3; i >= 0; i--){
-            a = nums[i];
-            b = nums[i+1];
-            c = nums[i+2];
-            if (a+b > c){
-                return a+b+c;
+            if (nums[i]+nums[i+1] > nums[i+2]){
+                return nums[i]+nums[i+1] + nums[i+2];
             }
         }
         return 0;
