2ac5f1315ec0a7570fc1719dca52569f93d861b9
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2025-10-28 13:19:05 +0100

    Better solution, looking for balanced positions that start with 0, dont run simulation explicitly. Beats 83%.

--- 3354/cpp/solution.cpp
+++ 3354/cpp/solution.cpp
@@ -2,42 +2,33 @@
 
 using namespace std;
 
-bool runSimulation(int currentPos, int dir, vector<int> nums){
-    // startPos is 0 is assumed!
-
-    while (currentPos >= 0 && currentPos < nums.size()){
-        if (nums[currentPos] == 0){
-            currentPos += dir;
-        }
-        else {
-            nums[currentPos]--;
-            dir *= -1;
-            currentPos += dir;
-        }
-    }
-
-    for (int num: nums){
-        if (num != 0){
-            return false;
-        }
-    }
-    return true;
-}
-
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int res_count = 0;
-        vector<int> dirs = {-1, 1};
-        for (int startPos = 0; startPos < nums.size(); startPos++){
-            if (nums[startPos] == 0){
-                for (int dir: dirs){
-                    if (runSimulation(startPos, dir, nums)){
-                        res_count++;
-                    }
-                }
-            }
+
+        int left = 0;
+        int right = 0;
+        for (int num: nums){
+            right += num;
         }
+
+        for (int i = 0; i < nums.size(); i++){
+
+            if (nums[i] == 0){
+                if (left == right){
+                    res_count += 2;
+                }
+                else if (left -1 == right || right -1 == left)
+                {
+                    res_count++;
+                }
+                
+            }
+            left += nums[i];
+            right -= nums[i];
+        }
+
         return res_count;
     }
 };
