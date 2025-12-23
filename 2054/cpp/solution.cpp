eedc7959350c1b0b164b46d599b1797401bd0207
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2025-12-23 12:33:09 +0100

    2x faster solution, still too slow. Beats 5.8%

--- 2054/cpp/solution.cpp
+++ 2054/cpp/solution.cpp
@@ -17,57 +17,39 @@ public:
     int maxTwoEvents(vector<vector<int>>& events) {
         
         map<int, int> max_before;
-        map<int, int> max_after;
 
         
         sort(events.begin(), events.end(), end_smaller);
-        int last_max = events[0][2];
+        int current_max = events[0][2];
         for (vector<int> event: events){
-            last_max = max(event[2], last_max);
-            max_before[event[1]] = last_max;
-            // max_before.insert({event[1], last_max});
-        }
-
-        sort(events.begin(), events.end(), start_greater);
-        last_max = events[0][2];
-        for (vector<int> event: events){
-            last_max = max(event[2], last_max);
-            max_after[event[0]] = last_max;
-            // max_after.insert({event[0], last_max});
+            current_max = max(current_max, event[2]);
+            max_before[event[1]] = current_max;
         }
 
         int res = 0;
         int current_res = 0;
-        int after = 0;
+        std::map<int, int>::iterator before;
         for (vector<int> event: events){
-            
-            auto after = max_after.upper_bound(event[1]);
-            if (after != max_after.end()){
-                current_res = after->second + event[2];
+            before = max_before.lower_bound(event[0]);
+
+            while (before != max_before.begin() && before->first >= event[0]){
+                before --;
+            }
+            if (before->first < event[0]){
+                current_res = event[2] + before->second;
             } else {
                 current_res = event[2];
             }
-
-            auto before = max_before.begin();
-            before = max_before.lower_bound(event[0]);
-            while (before != max_before.begin() && before->first >= event[0]){
-                before--;
-            }
-            if (before->first < event[0]){
-                current_res = max(current_res, before->second + event[2]);
-            }
-
             res = max(res, current_res);
         }
 
         return res;
 
-
     }
 };
 
 int main(){
-    vector<vector<int>> input = {{1,3,2},{4,5,2},{1,5,5}};
+    vector<vector<int>> input = {{35,90,47},{72,80,70}};
 
     Solution solver = Solution();
     solver.maxTwoEvents(input);
