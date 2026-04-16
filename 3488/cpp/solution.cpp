51fc733e0211f5bb4a273e899a9ff38394d971d2
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2026-04-16 15:05:40 +0200

    Solution checking only left and right element of current index. Second map necessary to know pos of neighbouring identical elements. Beats 14%.

--- 3488/cpp/solution.cpp
+++ 3488/cpp/solution.cpp
@@ -6,12 +6,16 @@ using namespace std;
 class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+
         unordered_map<int, vector<int>> nums_indices;
+        unordered_map<int, int> index_offsets;
         for (int i = 0; i < nums.size(); i++){
             if (nums_indices.find(nums[i]) != nums_indices.end()){
+                index_offsets[i] = nums_indices[nums[i]].size();
                 nums_indices[nums[i]].push_back(i);
             } else {
                 nums_indices[nums[i]] = {i};
+                index_offsets[i] = 0;
             }
         }
 
@@ -21,6 +25,7 @@ public:
         int idx;
         int dist;
         int a; int b;
+        int offset; int low; int high;
         for (int i = 0; i < queries.size(); i++){
             idx = queries[i];
             num = nums[idx];
@@ -28,16 +33,21 @@ public:
             if (nums_indices.find(num) == nums_indices.end() || nums_indices[num].size() == 1){
                 continue;
             }
-            dist = __INT32_MAX__;
-            for (int j: nums_indices[num]){
-                if (j == idx){
-                    continue;
-                }
+            offset = index_offsets[idx];
 
-                a = max(j, idx) - min(j, idx);
-                b = (min(j, idx) - max(j, idx) + n) % n;
-                dist = min(dist, min(a, b));
-            }
+            dist = __INT32_MAX__;
+            // Go to left
+            low = nums_indices[num][(offset - 1 + nums_indices[num].size()) % nums_indices[num].size()];
+            high = nums_indices[num][(offset + 1) % nums_indices[num].size()];
+
+            a = max(low, idx) - min(low, idx);
+            b = (min(low, idx) - max(low, idx) + n) % n;
+            dist = min(dist, min(a, b));
+
+            a = max(high, idx) - min(high, idx);
+            b = (min(high, idx) - max(high, idx)+n) % n;
+            dist = min(dist, min(a, b));
+            
             res[i] = dist;
 
         }
