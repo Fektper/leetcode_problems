97d14306705c5f64adfe0db43a95d96ced867788
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2026-06-09 09:40:56 +0200

    Changed iteration to 'max/min_element' call. Beats 100%.

--- 3689/cpp/solution.cpp
+++ 3689/cpp/solution.cpp
@@ -1,21 +1,13 @@
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        int maximum = 0;
-        int minimum = __INT32_MAX__;
+        int maximum = *max_element(nums.begin(), nums.end());
+        int minimum = *min_element(nums.begin(), nums.end());
 
-        for (int num: nums){
-            if (num > maximum){
-                maximum = num;
-            }
-            if (num < minimum){
-                minimum = num;
-            }
-        }
         long long int dif = maximum - minimum;
 
         return k * dif;
