64b4bd69622bd0e75c8bc24c88ec748eba46d3d1
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2026-07-20 13:44:00 +0200

    Reduced computation overhead by calculating new column only once per column of the grid instead of for each element. Beats 100%.

--- 1260/cpp/solution.cpp
+++ 1260/cpp/solution.cpp
@@ -10,16 +10,14 @@ public:
 
         vector<vector<int>> res = vector<vector<int>>(rows, vector<int>(cols, 0));
 
-        int pos; int i_new; int j_new;
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                pos = i * cols + j + k;
-                i_new = (pos / cols) % rows;
-                j_new = pos%cols;
-                res[i_new][j_new] = grid[i][j];
+        int delta_row; int j_new;
+        for (int j = 0; j < cols; j++){
+            delta_row = (j+k) / cols;
+            j_new = (j + k) % cols;
+            for (int i = 0; i < rows; i++){
+                res[(i + delta_row) % rows][j_new] = grid[i][j];
             }
         }
-
         return res;
 
     }
