0fbbcdfd6b07685b25bdba13bc2d6d46a52f708e
Andor Zenon Amandus Kristen <andor.zenon.amandus.kristen@hhi.fraunhofer.de>
2026-09-08 18:10:46 +0200

    Simple substract 999 from n. Each number has at most 1 comma. Beats 100%.

---
+++ 3870/rust/main.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn count_commas(n: i32) -> i32 {
+
+        if n < 1000{
+            return 0;
+        }
+        return n - 999;
+        if 
+
+        let mut res = 0;
+
+
+    }
+}
